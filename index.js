b29aa999c8a5d8eef42a735c7452c799b5c979dc
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-03 06:15:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true })); // Add this line to parse query strings
 
 app.get("/healthz", (_req, res) => res.send("ok"));
 app.use("/tts", chunkRouter);
