5ac2e01ccad26e05ca3b0d486bceca31b6c69a62
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-06 14:39:52 +0100

    Update saveToR2.js

--- saveToR2.js
+++ saveToR2.js
@@ -1,12 +1,23 @@
-const { S3Client } = require('@aws-sdk/client-s3');
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const s3 = new S3Client({
   region: 'auto',
   endpoint: process.env.R2_ENDPOINT,
   credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY,
-    secretAccessKey: process.env.R2_SECRET_KEY
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY
   }
 });
 
-module.exports = s3;
+export async function saveToR2(filename, fileBuffer) {
+  const uploadParams = {
+    Bucket: process.env.R2_BUCKET_NAME,
+    Key: filename,
+    Body: fileBuffer,
+    ContentType: 'audio/mpeg'
+  };
+
+  await s3.send(new PutObjectCommand(uploadParams));
+
+  return `${process.env.R2_PUBLIC_BASE_URL}/${filename}`;
+}
